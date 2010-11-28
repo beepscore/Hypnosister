d616d3d6d8256338ef859b4ce5182a7a0e66d4aa
beepscore <support@beepscore.com>
2010-11-27 23:26:48 -0800

    Add HypnosisView.

---
+++ Classes/HypnosisView.h
@@ -0,0 +1,16 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Steve Baker on 11/27/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HypnosisView : UIView {
+
+}
+
+@end
---
+++ Classes/HypnosisView.m
@@ -0,0 +1,37 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Steve Baker on 11/27/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+
+@implementation HypnosisView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end

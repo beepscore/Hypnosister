588d3c0cd25340006e2dda6c72f8ce95ab6cef3a
beepscore <support@beepscore.com>
2010-11-27 23:18:20 -0800

    Initial commit.  Reference "iPhone Programming - The Big Nerd Ranch Guide" by Conway and Hillegass Ch 06.

---
+++ Classes/HypnosisterAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Steve Baker on 11/27/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
